9e0f2d8fe6b2f2ebfd37f59ff5157534644bb9bf
med1245 <39544987+med1245@users.noreply.github.com>
2023-05-14 06:28:17 +0100

    repo cloudstream

---
+++ detectiveconanarProvider/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 2
+
+cloudstream {
+    authors = listOf( "mehdiMARSAMAN" )
+
+	language = "ar"
+
+    status = 1
+
+    tvTypes = listOf( "Anime" , "AnimeMovie" , "OVA" )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=detectiveconanar.com&sz=%size%"
+}
---
+++ detectiveconanarProvider/src/main/kotlin/com/detectiveconanar/detectiveconanarPlugin.kt
@@ -0,0 +1,12 @@
+package com.detectiveconanar
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DetectiveConanARPlugin : Plugin() {
+    override fun load(context: Context) {
+        
+    }
+}
